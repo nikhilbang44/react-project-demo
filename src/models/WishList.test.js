b0738c93a6718e03dff20ad029085d7926e39880
Nikhil Bang <nikhilbang44@gmail.com>
2019-01-06 23:17:15 +0530

    Test MST Models by Recording Snapshots or Patches
        Testing models is straightforward. Especially because MST provides powerful
    tools to track exactly how your state changes over time.Track snapshots, action
    invocations or even patches to verify the correctness of your actions!
    
    1. To obtain immutable snapshots of the state using getSnapshot
    2. To record snapshots using onSnapshot
    3. To store and test modifications over time using onPatch
    4. Using Jest's snapshot test feature to verify snapshots and patches
    5. That MST can infer the type of a snapshot for you

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,10 +1,12 @@
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree"
 import { WishList, WishListItem } from "./WishList"
+
 
 it("can create a instance of a model", () => {
     const item = WishListItem.create({
-        "name": "Glass",
-        "price": 28.73,
-        "image": "https://images.pexels.com/photos/1346155/pexels-photo-1346155.jpeg"
+        name: "Glass",
+        price: 28.73,
+        image: "https://images.pexels.com/photos/1346155/pexels-photo-1346155.jpeg"
     })
     
     expect(item.price).toBe(28.73)
@@ -26,6 +28,12 @@ it('can create a wishlist', () => {
 
 it('can add new items', () => {
     const list = WishList.create()
+    const states = []
+    
+    onSnapshot(list, snapshot => {
+        states.push(snapshot);
+    })
+
     list.add(
         WishListItem.create({
             name: "mobile",
@@ -36,5 +44,28 @@ it('can add new items', () => {
     expect(list.items[0].name).toBe("mobile")
     list.items[0].changeName("Book of saverkar")
     expect(list.items[0].name).toBe('Book of saverkar')
+    expect(getSnapshot(list)).toMatchSnapshot()
+    expect(states).toMatchSnapshot()
+})
+
+
+it('can add new items = 2', () => {
+    const list = WishList.create()
+    const patches = []
+    
+    onPatch(list, patch => {
+        patches.push(patch);
+    })
+
+    list.add(
+        WishListItem.create({
+            name: "mobile",
+            price: 10
+        })
+    )
+ 
+    list.items[0].changeName("Book of saverkar")
+
+    expect(patches).toMatchSnapshot()
 })
 
