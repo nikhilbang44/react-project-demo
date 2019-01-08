5275a8b0ec622d371b12c685ba0bb3e4029ab6e4
nikhilbang44 <nikhilbang44@gmail.com>
2019-01-08 15:56:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
-import Blog from './containers/Blog/Blog';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Blog />
       </div>
     );
   }
