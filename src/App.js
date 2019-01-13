6fa4c5c9510b6c34ac553dc5f9baf6b9a54a4b89
Nikhil Bang <nikhilbang44@gmail.com>
2019-01-13 14:17:58 +0530

    React Router (Route, Link, NavLink, Redirect, Prompt )
    Redirect to different route,
    Use links to route to specific page
    and use prompt to stop from exiting a route.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, NavLink, Link, Redirect, Prompt } from 'react-router-dom';
+import Route from 'react-router-dom/Route'
 
+const User = (params) => {
+  return (<h1> Welcome User { params.username }  </h1>)
+}
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
+  loginHandle = () => {
+    this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn 
+    }))
+  }
+
   render() {
     return (
-      <div className="App">
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <NavLink to='/' exact activeStyle={ { color: 'green'} }>Home</NavLink> 
+            </li>
+            <li>
+              <NavLink to="/about" exact activeStyle={ { color: 'green'} }>About</NavLink> 
+            </li>
+            <li>
+              <Link to="/user/Nikhil">User Nikhil</Link> 
+            </li>
+          </ul>
+
+          <Prompt
+            when={!this.state.loggedIn}
+            message={(location) => { return location.pathname.startsWith('/user') ? "Are you sure?" : true }}
+          />
+        <input type="button" value={this.state.loggedIn ? 'log out' : 'log in'} onClick={this.loginHandle.bind(this)}/>
+        
+        <Route path="/" exact strict render={
+          ()=> {
+            return(<h1>Welcome Home</h1>)
+          }
+        }></Route>
+        <Route path="/about" exact strict render={
+          ()=> {
+            return(<h1>Welcome About</h1>)
+          }
+        }></Route>
+        <Route path="/user/:username" exact strict render={
+          ({match})=>(
+          this.state.loggedIn ? (<User username={ match.params.username }/>) : (<Redirect to="/" />) 
+          )
+        }></Route>        
+        </div>
+      </Router>
     );
   }
 }
